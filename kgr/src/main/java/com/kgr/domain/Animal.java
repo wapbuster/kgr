98ccb6c4e845fa709e6ff9c10345e651f99167fa
wapbuster <wapbuster@gmail.com>
2019-04-15 19:50:28 -0400

    animals added

---
+++ kgr/src/main/java/com/kgr/domain/Animal.java
@@ -0,0 +1,23 @@
+package com.kgr.domain;
+
+public class Animal {
+
+    private String dog;
+    private String cat;
+
+    public String getDog() {
+        return dog;
+    }
+
+    public void setDog(String dog) {
+        this.dog = dog;
+    }
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
